6b36abe44b1db1f35b8d5561174619f14111c186
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-02 20:05:42 +0600

    Navbar design complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -26,32 +27,15 @@ const Navbar = () => {
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <Link> Home</Link>
             </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
+
             <li>
-              <a>Item 3</a>
+              <Link> Chef Recipes</Link>
+            </li>
+
+            <li>
+              <Link>Blog</Link>
             </li>
           </ul>
         </div>
@@ -60,37 +44,20 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <Link> Home</Link>
           </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
+
           <li>
-            <a>Item 3</a>
+            <Link> Chef Recipes</Link>
+          </li>
+
+          <li>
+            <Link>Blog</Link>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn btn-accent">Get started</a>
+        <Link className="btn btn-accent">Login</Link>
       </div>
     </div>
   );
