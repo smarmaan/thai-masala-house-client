0e04324f7495f4eb7c74e9f234e17d4ef948d4c4
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-02 18:18:41 +0600

    important packages installed... routes created...

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfHJqpWWEf83Je9493kedv_5nBDM1FJyI",
+  authDomain: "thai-masala-house.firebaseapp.com",
+  projectId: "thai-masala-house",
+  storageBucket: "thai-masala-house.appspot.com",
+  messagingSenderId: "12481189617",
+  appId: "1:12481189617:web:221a3a03fe192222423b50",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
