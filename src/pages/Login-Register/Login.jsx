d3c5eb48991bd58de5f7a1a12d7ae54c5633f936
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-03 16:05:31 +0600

    Google and Github button added...

--- src/pages/Login-Register/Login.jsx
+++ src/pages/Login-Register/Login.jsx
@@ -7,9 +7,12 @@ import { AuthContext } from "../../providers/AuthProvider";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
-  const { signIn } = useContext(AuthContext);
+  const { signIn, user } = useContext(AuthContext);
 
   const [passwordType, setPasswordType] = useState("password");
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const navigate = useNavigate();
 
@@ -26,8 +29,8 @@ const Login = () => {
 
     console.log(email, password);
 
-    // setError("");
-    // setSuccess("");
+    setError("");
+    setSuccess("");
 
     signIn(email, password)
       .then((result) => {
@@ -35,12 +38,11 @@ const Login = () => {
         console.log(loggedUser);
         form.reset();
         navigate(from, { replace: true });
-
-        // setSuccess("Login Successful");
+        setSuccess("Login Successful");
       })
       .catch((error) => {
         console.error(error);
-        // setError(error.message);
+        setError(error.message);
       });
   };
 
@@ -48,8 +50,11 @@ const Login = () => {
     setPasswordType(passwordType === "password" ? "text" : "password");
   };
 
+  console.log(error);
+  console.log(success);
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 rounded-3xl my-6">
+      {/* min-h-screen  */}
       <div className="hero-content flex-col ">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login</h1>
@@ -81,31 +86,69 @@ const Login = () => {
                 required
                 className="input input-bordered"
               />
-              <button className="my-2 bg-red-200" onClick={togglePassword}>
+              <div className="my-2" onClick={togglePassword}>
                 {passwordType === "password" ? (
-                  <div className="flex items-center gap-2 justify-end">
-                    <FaEye></FaEye>Show PassWord{" "}
+                  <div className="flex items-center gap-2 justify-end cursor-pointer">
+                    <FaEye></FaEye>
+                    Show PassWord
                   </div>
                 ) : (
-                  <div className="flex items-center gap-2 justify-end">
-                    <FaEyeSlash /> Hide PassWord{" "}
+                  <div className="flex items-center gap-2 justify-end cursor-pointer">
+                    <FaEyeSlash />
+                    Hide PassWord
                   </div>
                 )}
-              </button>
+              </div>
               <label className="label">
                 <Link to="/register" className="label-text-alt link link-hover">
                   Forgot password?
                 </Link>
               </label>
             </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+            {/* <div className="form-control">
+              <button className="btn btn-sm btn-primary">Login</button>
+            </div>
+            <div className="form-control">
+              <button className="btn btn-sm btn-primary">Google</button>
+            </div>
+            <div className="form-control">
+              <button className="btn btn-sm btn-primary">Github</button>
+            </div> */}
+
+            <div className="form-control">
+              <button className="btn btn-sm text-black hover:text-white bg-orange-200 hover:bg-orange-400 ">
+                Login
+              </button>
+            </div>
+
+            <div className="flex items-center justify-center gap-3 my-3">
+              <div className="form-control">
+                <button className="btn btn-sm bg-orange-200 hover:bg-orange-400 ">
+                  <img
+                    className="w-5"
+                    src="../../../public/search.png"
+                    alt=""
+                  />
+                </button>
+              </div>
+              <div className="form-control">
+                <button className="btn btn-sm  bg-orange-200 hover:bg-orange-400">
+                  <img
+                    className="w-5 bg-white rounded-full"
+                    src="../../../public/github.png"
+                    alt=""
+                  />{" "}
+                </button>
+              </div>
             </div>
 
             <p>
               New to EMA-JHON?{" "}
               <span>
-                <Link to="/register" className="btn btn-xs btn-primary">
+                <Link
+                  to="/register"
+                  className="btn btn-xs  text-black hover:text-white bg-orange-200 hover:bg-orange-400 "
+                >
                   Register
                 </Link>
               </span>
