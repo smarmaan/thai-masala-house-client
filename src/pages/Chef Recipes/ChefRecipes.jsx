fcce77ff7446813daa24fea2b1b3e939c378ebae
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-04 01:16:17 +0600

    chef data load done in banner

--- src/pages/Chef Recipes/ChefRecipes.jsx
+++ src/pages/Chef Recipes/ChefRecipes.jsx
@@ -15,14 +15,48 @@ const ChefRecipes = () => {
       .then((data) => setChefs(data));
   }, []);
 
-  // console.log(chefs);
-
   const findID = chefs.find((chef) => chef.id == id);
   console.log(findID);
+
+  // id, name, picture, yearsOfExperience, numberOfRecipes, likes
+
+  const name = findID?.name;
+  const picture = findID?.picture;
+  const yearsOfExperience = findID?.yearsOfExperience;
+  const numberOfRecipes = findID?.numberOfRecipes;
+  const likes = findID?.likes;
+  // console.log(name, picture, yearsOfExperience, numberOfRecipes, likes);
 
   return (
     <div>
       <p>ChefRecipes : {chefRecipes.length}</p>
+
+      {/* 
+
+
+
+ */}
+
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={picture} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">{name}</h1>
+            <p className="py-2">short bio/description</p>
+            <p className="py-2">{yearsOfExperience}</p>
+            <p className="py-2">{numberOfRecipes}</p>
+            <p className="py-2">{likes}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* 
+
+
+
+
+
+ */}
 
       {chefRecipes.map((recipe) => (
         <RecipesCard key={recipe.id} recipe={recipe} />
