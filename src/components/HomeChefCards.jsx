6af6a31263083e01b0c6f17bbe7193a500998abe
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-05 09:25:05 +0600

    home chef card style changed..

--- src/components/HomeChefCards.jsx
+++ src/components/HomeChefCards.jsx
@@ -10,7 +10,7 @@ const HomeChefCards = ({ chef }) => {
   //   console.log(id, name, picture, yearsOfExperience, numberOfRecipes, likes);
 
   return (
-    <div className="card bg-orange-200 hover:bg-orange-300 shadow-xl my-10 mx-auto">
+    <div className="card font-serif bg-orange-200 hover:bg-orange-300 shadow-xl my-10 mx-auto">
       <LazyLoad
         height={`100%`}
         width={`100%`}
@@ -19,17 +19,21 @@ const HomeChefCards = ({ chef }) => {
           console.log("Chef Picture loaded! with lazy loader");
         }}
       >
-        <figure className="px-10  pt-10 mx-auto">
-          <img src={picture} alt="" className="rounded-xl w-96 h-96   shadow-2xl" />
+        <figure className="px-10 pt-10 mx-auto">
+          <img
+            src={picture}
+            alt=""
+            className="rounded-xl w-96 h-96   shadow-2xl"
+          />
         </figure>
       </LazyLoad>
 
       <div className="card card-body items-center text-center">
-        <h2 className="card-title font-bold text-2xl  bg-orange-300 rounded-3xl px-6 py-3 my-5 shadow-2xl">
+        <h2 className="card-title font-bold text-2xl  hover:text-3xl  bg-orange-300 rounded-3xl hover:bg-orange-400  px-6 py-3 my-5 shadow-2xl">
           Name: {name}
         </h2>
 
-        <div className="text-center text-lg font-semibold bg-orange-300 rounded-3xl px-10 py-3 my-5 shadow-2xl">
+        <div className="text-center text-lg hover:text-2xl font-semibold bg-orange-300 rounded-3xl hover:bg-orange-400 px-10 py-3 my-5 shadow-2xl">
           <p className="">Years of Experience: {yearsOfExperience}</p>
           <p className="">Number of Recipes: {numberOfRecipes}</p>
           <p className="">Likes : {likes}</p>
@@ -37,7 +41,7 @@ const HomeChefCards = ({ chef }) => {
 
         <div className="card-actions ">
           <Link to={`/chef/${chef.id}`}>
-            <button className="btn text-black border-0 shadow-xl hover:text-white normal-case text-xl font-bold  bg-orange-300 hover:bg-orange-400 ">
+            <button className="btn text-black border-0 shadow-xl hover:text-white normal-case text-xl hover:text-2xl font-bold  bg-orange-300 hover:bg-orange-400 ">
               View Recipes
             </button>
           </Link>
